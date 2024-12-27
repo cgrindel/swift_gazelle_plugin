bab6a1949fd31d4bd900021e09d4b0b10b383dcf
Logan Shire <logan.shire@gmail.com>
2024-12-27 12:59:09 -0300

    Revert package name change

--- examples/grpc_package_example/Package.swift
+++ examples/grpc_package_example/Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "grpc_example",
+    name: "grpc_package_example",
     dependencies: [
         // These are the versions used by rules_swift
         .package(url: "https://github.com/grpc/grpc-swift.git", exact: "1.16.0"),
