b52e3f918f9ab8f5e621dc932c67823f9a0f61c2
Chuck Grindel <chuck@revealtech.ai>
2024-12-28 12:07:13 -0700

    Tests are green.

---
+++ tools/generate_ci_workflow/internal/testparams/int_test_params.go
@@ -0,0 +1,30 @@
+package testparams
+
+import "encoding/json"
+
+const MacOS = "macos"
+const LinuxOS = "linux"
+
+type IntTestParams struct {
+	Test string `json:"test"`
+	OS   string `json:"os"`
+}
+
+func (itp *IntTestParams) Runner() string {
+	switch itp.OS {
+	case MacOS:
+		return "macos-13"
+	case LinuxOS:
+		return "ubuntu-22.04"
+	default:
+		return ""
+	}
+}
+
+func NewIntTestParamsFromJSON(b []byte) ([]IntTestParams, error) {
+	var intTestParams []IntTestParams
+	if err := json.Unmarshal(b, &intTestParams); err != nil {
+		return nil, err
+	}
+	return intTestParams, nil
+}
